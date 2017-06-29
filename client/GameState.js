144da07cd94be667d712e5cdbb11a18d8db81b69
Sittipong Jungsakul <sittipongwork@gmail.com>
2017-06-29 15:19:08 +0700

    Add infomation comment

--- client/GameState.js
+++ client/GameState.js
@@ -1,15 +1,18 @@
-
+//สร้าง object State สำหรับจัดการ state ของตัวเกม
 var gameState = new Kiwi.State( "GameState" );
 
+//preload คือ การเตรียมข้อมูลไว้ก่อนที่ state จะสร้างตัวเกม
 gameState.preload = function(){
     this.game.stage.color = '#fff';
 	Kiwi.State.prototype.preload.call(this);
 }
 
+//create คือ การสร้างตัวเกมเริ่มต้น
 gameState.create = function(){
 	Kiwi.State.prototype.create.call(this);
 }
 
+//update คือ ฟังก์ชันสำหรับ Listening ตัวเกม เวลามีการเปลี่ยนแปลงไดๆ
 gameState.update = function(){
 	Kiwi.State.prototype.update.call(this);
 }
