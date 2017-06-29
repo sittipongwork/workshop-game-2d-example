207ebd23a231d59d5c239e9d14e151742cd81846
sittipongwork <sittipongwork@gmail.com>
2017-06-29 20:28:03 +0700

    add tile map to game

--- client/GameState.js
+++ client/GameState.js
@@ -5,16 +5,27 @@ var gameState = new Kiwi.State( "GameState" );
 gameState.preload = function(){
     this.game.stage.color = '#000';
 	Kiwi.State.prototype.preload.call(this);
-
+	//โหลด Background
 	this.addImage('bgstate','client/resource/map/Stage1/bg-stage1.jpg')
+	
+	//โหลด TileMap
+	this.addSpriteSheet( 'tiles', 'client/resource/map/Stage1/starmap1.png', 48, 48 );
+	this.addJSON('tilemap', 'client/resource/map/Stage1/TileMap.json');
+	
 }
 
 //create คือ การสร้างตัวเกมเริ่มต้น
 gameState.create = function(){
 	Kiwi.State.prototype.create.call(this);
-
+	//สร้าง Map Background
 	this.bgstate = new Kiwi.Plugins.GameObjects.RepeatingTexture(this,this.textures.bgstate,-800,-500,20000,1300);
 	this.addChild(this.bgstate);
+
+	//สร้าง Tile Map
+	this.tilemap = new Kiwi.GameObjects.Tilemap.TileMap( this, 'tilemap', this.textures.tiles );
+	this.addChild( this.tilemap.layers[0] );
+	//กำหนด ArcadePhysics ของ map เพื่อบ่งบอกว่าสิ่งนี้คือวัตถุในเกม
+	this.tilemap.tileTypes[0].allowCollisions = Kiwi.Components.ArcadePhysics.ANY;
 }
 
 //update คือ ฟังก์ชันสำหรับ Listening ตัวเกม เวลามีการเปลี่ยนแปลงไดๆ
